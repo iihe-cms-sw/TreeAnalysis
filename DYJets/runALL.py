75567c1dbc9b48561b28a19d55fb0afd917d4548
Alexandre Léonard <aleonard@cern.ch>
2013-11-01 11:37:38 +0100

    *.log should be ignored, and runALL.py has been improved

--- DYJets/runALL.py
+++ DYJets/runALL.py
@@ -1,13 +1,58 @@
 #!/usr/bin/env python
 import os, sys, string
 import re, time
-lepton="DE"
-PT="30"
-for i in  range(1, 3):
- cmd1="sed -e \"s|.*int doWhat.*|int doWhat = "+str(i)+";|g\" " +"-e \"s|.*string lepSelection.*|string lepSelection = "+str(lepton)+";|g\" " +  " -e \"s|.*int jetPtMin.*| int jetPtMin = "+str(PT) +";|g\"  runDYJets.cc > sranje.cc "
- os.system(cmd1) 
- cmd2="nohup root -b -q sranje.cc++  >& LOG_Lep"+str(lepton)+"_option"+str(i)+"_PT"+str(PT)+".txt & \n"
- print cmd2
- os.system(cmd2)
- time.sleep(120)
- 
+
+#--------------------------------------------------------------------------------
+def loopHasNotStarted(logName):
+
+    command = "cat " + logName + " > tmp.log"
+    os.system(command)
+
+    catFile = open("tmp.log","r")
+    for line in catFile:
+        if "Processing :" in line:
+            os.system("rm tmp.log")
+            return false
+
+
+    os.system("rm tmp.log")
+    return true
+
+#--------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------
+def dispacher(lepton = "DE", PT = 30):
+
+    for i in range(1, 3):
+        # Create the .cc file based on runDYJets.cc
+        tmpFile = "tmp_" + str(lepton) + "_" + str(PT) + "_" + str(i)
+        tmpCCFile = tmpFile + ".cc"
+        tmpLogFile = tmpFile + ".log"
+
+        cmdToSetJob = "sed "
+        cmdToSetJob += " -e \"s|.*int doWhat.*|int doWhat = " + str(i) + ";|g\""
+        cmdToSetJob += " -e \"s|.*string lepSelection.*|string lepSelection = " + str(lepton) + ";|g\""
+        cmdToSetJob += " -e \"s|.*int jetPtMin.*|int jetPtMin = " + str(PT) + ";|g\""
+        cmdToSetJob += " runDYJets.cc > " + tmpCCFile
+
+        print cmdToSetJob
+        os.system(cmdToSetJob)
+
+        cmdToExecJob = "nohup root -b -q " + tmpCCFile + "++ &> " + tmpLogFile + " &"
+        print cmdToExecJob
+        os.system(cmdToExecJob)
+
+        # Wait for the loop to start before going to next job
+        while loopHasNotStarted(tmpLogFile):
+            time.sleep(2)
+
+#--------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------
+if __name__ == "__main__":
+    dispacher("DE", 30)
+    #dispacher("DE", 20)
+
+#--------------------------------------------------------------------------------
