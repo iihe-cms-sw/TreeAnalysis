e84ba3360305a1f03438ed6b24a9a8c83d12fb64
Alexandre Léonard <aleonard@cern.ch>
2014-09-03 12:59:59 +0200

    changing compiler for better thread usage

--- DYJets/dispatcher.cc
+++ DYJets/dispatcher.cc
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <thread>
 
-void runZJets(std::string option) 
+void runZJets(std::string option, std::string machine) 
 {
     std::string command = "./runZJets " + option;
+    if (machine != "") command = "ssh " + machine + " \'source .bash_profile; cd TreeAnalysis2012/DYJets/; " + command +"\'";  
+    //std::string command = "./runZJets " + option;
     std::cout << command << std::endl;
     system(command.c_str());
 }
@@ -11,30 +13,38 @@ void runZJets(std::string option)
 int main(int argc, char **argv)
 {
 
-    std::thread t[5];
+    const static int NDYJETSTHREAD = 10;
+    std::thread DYJetsThread[NDYJETSTHREAD];
 
 
     auto start = std::chrono::system_clock::now();
     std::cout << "Starting to dispatch" << std::endl;
 
-    t[0] = std::thread(runZJets, "lepSel=DMu whichSyst=0");
-    std::cout << "Central submitted" << std::endl;
+    for (int i = 0; i < 2; ++i) {
+        std::string lep = "DE";
+        if (i == 1) lep = "DMu";
 
-    t[1] = std::thread(runZJets, "lepSel=DMu whichSyst=1");
-    std::cout << "Syst down submitted" << std::endl;
+        DYJetsThread[i*5 + 0] = std::thread(runZJets, "lepSel=" + lep + " whichSyst=0", "");
+        std::cout << "Central submitted" << std::endl;
 
-    t[2] = std::thread(runZJets, "lepSel=DMu whichSyst=2");
-    std::cout << "Syst up submitted" << std::endl;
+        DYJetsThread[i*5 + 1] = std::thread(runZJets, "lepSel=" + lep + " whichSyst=1", "m7");
+        std::cout << "Syst down submitted" << std::endl;
 
-    t[3] = std::thread(runZJets, "lepSel=DMu whichSyst=3");
-    std::cout << "Syst down submitted" << std::endl;
+        DYJetsThread[i*5 + 2] = std::thread(runZJets, "lepSel=" + lep + " whichSyst=2", "m7");
+        std::cout << "Syst up submitted" << std::endl;
 
-    t[4] = std::thread(runZJets, "lepSel=DMu whichSyst=4");
-    std::cout << "Syst up submitted" << std::endl;
+        DYJetsThread[i*5 + 3] = std::thread(runZJets, "lepSel=" + lep + " whichSyst=3", "m8");
+        std::cout << "Syst down submitted" << std::endl;
+
+        DYJetsThread[i*5 + 4] = std::thread(runZJets, "lepSel=" + lep + " whichSyst=4", "m8");
+        std::cout << "Syst up submitted" << std::endl;
+
+    }
+
 
     //Join the threads with the main thread
-    for (int i = 0; i < 5; ++i) {
-        t[i].join();
+    for (int i = 0; i < NDYJETSTHREAD; ++i) {
+        DYJetsThread[i].join();
     }
     std::cout << "All jobs done" << std::endl;
 
