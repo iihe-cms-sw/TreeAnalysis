8b11d8884b0534971d7a757c034215d1beaa46d2
Alexandre Leonard <aleonard@cern.ch>
2014-09-03 09:21:41 +0200

    more syst in dispatcher

--- DYJets/dispatcher.cc
+++ DYJets/dispatcher.cc
@@ -16,14 +16,20 @@ int main(int argc, char **argv)
 
     auto start = std::chrono::system_clock::now();
     std::cout << "Starting to dispatch" << std::endl;
-    t[0] = std::thread(runZJets, "whichSyst=0");
-    //runZJets("doCentral=1 doSysRunning=0");
+
+    t[0] = std::thread(runZJets, "lepSel=DMu whichSyst=0");
     std::cout << "Central submitted" << std::endl;
-    t[1] = std::thread(runZJets, "whichSyst=1");
-    //runZJets("whichSyst=1");
+
+    t[1] = std::thread(runZJets, "lepSel=DMu whichSyst=1");
     std::cout << "Syst down submitted" << std::endl;
-    t[2] = std::thread(runZJets, "whichSyst=2");
-    //runZJets("whichSyst=2");
+
+    t[2] = std::thread(runZJets, "lepSel=DMu whichSyst=2");
+    std::cout << "Syst up submitted" << std::endl;
+
+    t[3] = std::thread(runZJets, "lepSel=DMu whichSyst=3");
+    std::cout << "Syst down submitted" << std::endl;
+
+    t[4] = std::thread(runZJets, "lepSel=DMu whichSyst=4");
     std::cout << "Syst up submitted" << std::endl;
 
     //Join the threads with the main thread
@@ -35,7 +41,7 @@ int main(int argc, char **argv)
 
     auto end = std::chrono::system_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << elapsed.count() << '\n';
+    std::cout << elapsed.count() << " ms\n";
 
     return 0;
 }
